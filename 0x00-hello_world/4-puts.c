f5f4908f1fecca4477e59a36f4cc37be31c7dfe7
Franktheu <franktheu756@gmail.com>
2022-09-09 06:43:32 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return: Always 0 (done)
  */
-int main ()
+int main (void)
 {
-	puts("\n"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
