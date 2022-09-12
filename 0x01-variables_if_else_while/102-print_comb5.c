619420b6cb94479185ae0c82e1ab9c2f2665578a
Franktheu <franktheu756@gmail.com>
2022-09-12 16:06:55 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - printing combinations of two 2-digits
+ *
+ * Return:Always(successful)
+ */
+int main(void)
+{
+	int i, p;
+
+	for (i = 0; i <= 100; i++)
+	{
+		for (p = 0; p <= 100; p++)
+		{
+			if (p > i)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((p / 10) + '0');
+				putchar((p % 10) + '0');
+				if (i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
