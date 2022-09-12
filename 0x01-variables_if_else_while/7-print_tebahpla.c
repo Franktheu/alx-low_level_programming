1774bd9a602d6b23a7b674cb5475c5d0e2e651c5
Franktheu <franktheu756@gmail.com>
2022-09-12 14:45:56 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing a-z in reverse
+ *
+ * Return: Always(successful)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
