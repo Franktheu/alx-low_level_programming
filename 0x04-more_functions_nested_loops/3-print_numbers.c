58bc3c5651dc7fa989d25e06138afce7b43606ef
Franktheu <franktheu756@gmail.com>
2022-09-16 16:58:46 -0700

    all tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - print numbers from 0-9
+ * Return: return nothing
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 58; n<68; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
